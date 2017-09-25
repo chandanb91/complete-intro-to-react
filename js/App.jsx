22698d23f62ba6799d4a075fcd792f12c2476c36
Chandan Gowda <chandan7791@gmail.com>
2017-09-25 20:20:45 +0530

    hot module setup

---
+++ js/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Landing from './Landing';
+import Search from './Search';
+
+const fourOnFour = () => <h1>404</h1>;
+
+const App = () => (
+  <BrowserRouter>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/search" component={Search} />
+        <Route component={fourOnFour} />
+      </Switch>
+    </div>
+  </BrowserRouter>);
+
+export default App;
